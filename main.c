6af2bf47c8a7e24751d21fa2a904fc3d4dc05742
oofdere <43102807+oofdere@users.noreply.github.com>
2021-08-21 00:15:22 -0700

    Draw a board

--- main.c
+++ main.c
@@ -1,83 +1,55 @@
-/*******************************************************************************************
-*
-*   raylib [core] example - Basic 3d example
-*
-*   Welcome to raylib!
-*
-*   To compile example, just press F5.
-*   Note that compiled executable is placed in the same folder as .c file
-*
-*   You can find all basic examples on C:\raylib\raylib\examples folder or
-*   raylib official webpage: www.raylib.com
-*
-*   Enjoy using raylib. :)
-*
-*   This example has been created using raylib 1.0 (www.raylib.com)
-*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
-*
-*   Copyright (c) 2013-2020 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
+#include <raylib.h>
 
-#include "raylib.h"
-
-int main() 
+int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    int screenWidth = 1920;
+    int screenHeight = 1080;
 
-    InitWindow(screenWidth, screenHeight, "raylib");
-
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 8.0f };
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+    InitWindow(screenWidth, screenHeight, "Heatchess Indev");
     
-    SetCameraMode(camera, CAMERA_ORBITAL);
+    SetTargetFPS(144);
 
-    Vector3 cubePosition = { 0 };
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    // gameplay loop
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera);
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
-
             ClearBackground(RAYWHITE);
+            
+            // keep window size up to date
+            if (IsWindowResized)
+            {
+                screenWidth = GetScreenWidth(); 
+                screenHeight = GetScreenHeight();
+            }
 
-            BeginMode3D(camera);
-
-                DrawCube(cubePosition, 2.0f, 2.0f, 2.0f, RED);
-                DrawCubeWires(cubePosition, 2.0f, 2.0f, 2.0f, MAROON);
-                DrawGrid(10, 1.0f);
-
-            EndMode3D();
-
-            DrawText("This is a raylib example", 10, 40, 20, DARKGRAY);
-
-            DrawFPS(10, 10);
-
+            // draw the chess board
+            bool flip = true; // square color
+            float scale = screenHeight / 8; // size of squares
+            int base = screenWidth / 2 - screenHeight / 2; // place to start drawing from
+            for (int y = 0; y < 8; y++)
+            {
+                int sy = scale * y;
+                flip = !flip;
+                for (int x = 0; x < 8; x++)
+                {
+                    int sx = scale * x + base;
+                    if (flip)
+                    {
+                        DrawRectangle(sx, sy, scale, scale, RED);
+                        flip = false;
+                    }
+                    else
+                    {
+                        DrawRectangle(sx, sy, scale, scale, BLACK);
+                        flip = true;
+                    }
+                }
+            }
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    
+    CloseWindow();
 
     return 0;
 }
